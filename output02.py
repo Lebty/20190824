903b90c88fce752515f26f6ca75d94095bb43447
Lebty <52067172+Lebty@users.noreply.github.com>
2019-08-24 12:19:09 +0900

    Add files via upload

---
+++ output02.py
@@ -0,0 +1,4 @@
+yes_votes = 42_252_654
+no_votes = 43_132_443
+percentage = yes_votes / (yes_votes + no_votes)
+'{:-9} YES votes  {:2.2%}'.format(yes_votes, percentage)
---
+++ output03.py
@@ -0,0 +1,5 @@
+a = [1, 2, 3, 4]
+b = ["first", "second", "third", "fourth"]
+c = a + b
+
+print("{0} {1} {2}" .format(a, b, c))
